0402adfb311c5760b82d4db6da6d969cd7fe7b8f
kostarasx <33222110+kostarasx@users.noreply.github.com>
2018-08-10 03:47:47 +0300

    4 - Switching Scenes
    Playing with scenes

--- src/JavaPractice.java
+++ src/JavaPractice.java
@@ -6,33 +6,64 @@ import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
+import javafx.application.*;
+import javafx.stage.*;
+import javafx.scene.*;
+import javafx.scene.layout.*;
+import javafx.scene.control.*;
+
 public class JavaPractice extends Application {
-	
-	 Button button;
-	
-	public static void main(String[] args) {
-		 launch(args);
+	Stage window;
+    Scene scene1, scene2, scene3;
 
-	}
+    public static void main(String[] args) {
+        launch(args);
+    }
 
-	@Override
-	public void start(Stage primaryStage) throws Exception {
-		primaryStage.setTitle("Title of the Window");
-		button = new Button();
-		button.setText("Click me");
-		
-		button.setOnAction(e -> System.out.println("This a lamda expression"));
-		
-		StackPane layout = new StackPane();
-		layout.getChildren().add(button);
-		
-		Scene scene = new Scene(layout, 300, 250);
-		primaryStage.setScene(scene);
-		primaryStage.show();
-	}
+    @Override
+    public void start(Stage primaryStage) {
+    	window = primaryStage;
+    	
+    	
+        //Button 1
+        Label label1 = new Label("Welcome to the first scene!");
+        Button button1 = new Button("Go to scene 2");
+        button1.setOnAction(e -> window.setScene(scene2));
+
+        //Layout 1 - children laid out in vertical column
+        VBox layout1 = new VBox(20);
+        layout1.getChildren().addAll(label1, button1);
+        scene1 = new Scene(layout1, 200, 200);
 
 
+        //Button 2
+        Button button2 = new Button("This sucks, go back to scene");
+        button2.setOnAction(e -> window.setScene(scene3));
 
-	
+        //Layout 2
+        StackPane layout2 = new StackPane();
+        layout2.getChildren().add(button2);
+        scene2 = new Scene(layout2, 600, 300);
+
+        //Button 3
+        
+        Label  label2 = new Label("AM i right?");
+        Button button3 = new Button ("Just me testing");
+        button3.setOnAction(e -> window.setScene(scene1));
+        
+        //Layout 3
+        VBox layout3 = new VBox(100);
+        layout3.getChildren().addAll(label2, button3);
+        scene3 = new Scene(layout3, 1000, 1000);
+        
+        //Display scene 1 at first
+        window.setScene(scene2);
+        window.setTitle("Title Here");
+        window.show();
+    }
 
 }
+
+
+	
+
